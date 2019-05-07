fd156a49c50f2d8d94c7b5d71616d30ded721225
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2019-05-07 16:19:11 +0200

    Add clickable shape

--- app/src/main/java/com/example/hittheshape/PlayActivity.java
+++ app/src/main/java/com/example/hittheshape/PlayActivity.java
@@ -1,13 +1,45 @@
 package com.example.hittheshape;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PlayActivity extends AppCompatActivity {
 
+    private Button shapeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        shapeButton = (Button)findViewById(R.id.shape);
+    }
+
+
+
+    public void hitTheShape(View view) {
+        Toast.makeText(this, "Shape pressed", Toast.LENGTH_LONG).show();
+        shapeButton.setClickable(false);
+        shapeButton.setVisibility(View.GONE);
+
+        float buttonX=shapeButton.getX()+50;
+        float buttonY=shapeButton.getY()+50;
+        shapeButton.setX(buttonX);
+        shapeButton.setY(buttonY);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                shapeButton.setVisibility(View.VISIBLE);
+                shapeButton.setClickable(true);
+            }
+    }, 2000);
+
+
     }
 }
