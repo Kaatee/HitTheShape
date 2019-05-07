704baf71fedd1bee444cfefac7f6e28ba07179cb
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2019-05-07 20:09:47 +0200

    Add randomize location

--- app/src/main/java/com/example/hittheshape/PlayActivity.java
+++ app/src/main/java/com/example/hittheshape/PlayActivity.java
@@ -1,15 +1,24 @@
 package com.example.hittheshape;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class PlayActivity extends AppCompatActivity {
 
     private Button shapeButton;
+    private int points=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,28 +26,67 @@ public class PlayActivity extends AppCompatActivity {
         shapeButton = (Button)findViewById(R.id.shape);
     }
 
+    private static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
 
+    private static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
 
+    private int getNavigationBarHeight() {
+        boolean hasMenuKey = ViewConfiguration.get(this).hasPermanentMenuKey();
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0 && !hasMenuKey)
+        {
+            return getResources().getDimensionPixelSize(resourceId);
+        }
+        return 0;
+    }
+
+    private int convertDpToPx(float valueDp){
+
+        float density = getResources().getDisplayMetrics().density;
+        float px = valueDp * density;
+        return (int)px;
+
+    }
+
+    private int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    private void updateTextView() {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(Integer.toString(points));
+    }
     public void hitTheShape(View view) {
-        Toast.makeText(this, "Shape pressed", Toast.LENGTH_LONG).show();
         shapeButton.setClickable(false);
         shapeButton.setVisibility(View.GONE);
 
-        float buttonX=shapeButton.getX()+50;
-        float buttonY=shapeButton.getY()+50;
-        shapeButton.setX(buttonX);
-        shapeButton.setY(buttonY);
+        points+=10;
+        updateTextView();
+
+        Random r = new Random();
+        int newX= r.nextInt(getScreenWidth()-shapeButton.getWidth()-20)+10;
+        int newY = r.nextInt(getScreenHeight()-shapeButton.getHeight()-getNavigationBarHeight()-getStatusBarHeight()-20)+10;
+
+        shapeButton.setX(newX);
+        shapeButton.setY(newY);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-
-
                 shapeButton.setVisibility(View.VISIBLE);
                 shapeButton.setClickable(true);
             }
-    }, 2000);
+    }, 500);
 
 
     }
